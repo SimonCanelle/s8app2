e1b5a118c229cab33d91882fd9f70f02986f210d
SimonCanelle <simon.j-bombardier@hotmail.ca>
2026-09-25 15:02:26 -0400

    improvement du choix de grandeur de tableau + initialisation du tableau d'encodage

--- QV_encode.py
+++ QV_encode.py
@@ -3,30 +3,40 @@ import numpy as np
 def QV_encode(Img_reduced, bitPerPixelGoal):
     #1. calcul des tailles 
     nPix = int(len(Img_reduced) * len(Img_reduced[0]))
-    nPixPerVec = 3 #choisis arbitrairement
     bitPerPix = 8 #image a été préprocess pour répondre à ça
-    nVec = nPix / nPixPerVec
+    nPixPerVec = 0
+    nBitPerInd = 0
     closest = np.inf
-    closestInd = 0
-    for i in range(2,10):
-        # la logique du diviseur est en combien de groupe on veux séparer nos vecteur 
-        # en assumant que chaque vecteur est différent
-        nIndex = (2**i)
-        dataSize = nVec*nIndex
-        metadataSize = bitPerPix*nPixPerVec*nIndex
-        loopBitPerPix = (dataSize+metadataSize)/nPix
-        distance = np.abs(bitPerPixelGoal - loopBitPerPix)
-        if (distance < closest):
-            closest = distance
-            closestInd = i
+    for j in range(2,8):
+        nVec = nPix / j        
+        for i in range(2,10):            
+            nIndex = (2**i)
+            dataSize = nVec*i
+            metadataSize = bitPerPix*nPixPerVec*nIndex + 64 #header: taille image source[16,16] + taille tableau encodage[16,16]
+            # la logique du diviseur est en combien de groupe on veux séparer nos vecteur 
+            # en assumant que chaque vecteur est différent
+            loopBitPerPix = (dataSize+metadataSize)/nPix
+            distance = np.abs(bitPerPixelGoal - loopBitPerPix)
+            if (distance < closest):
+                closest = distance
+                nBitPerInd = i
+                nPixPerVec = j
 
-    nIndex = (2**closestInd)
-    dataSize = nVec*nIndex
-    metadataSize = bitPerPix*nPixPerVec*nIndex
+    nVec = nPix / nPixPerVec
+    dataSize = nVec*nBitPerInd
+    metadataSize = bitPerPix*nPixPerVec*(2**nBitPerInd)
     realBitPerPix = (dataSize+metadataSize)/nPix
     
-    #2. 
-
+    #2. initialisation du tableau d'encodage de manière linéaire
+    nIndex = 2**nBitPerInd
+    encTab = np.zeros((nIndex,nPixPerVec))
+    line = np.linspace(0,255,nIndex)
+    for i in range(0,nIndex):
+        n = int(np.round(line[i]))
+        encTab[i] = np.full(nPixPerVec,n)
+    
+    #3. encodage de l'image
+    #3.1 vectorisation de l'image
 
 
 
